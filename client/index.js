e4cf779137741b3aeb269dc9371aaba265d8c430
rafbatista <rah5024@gmail.com>
2017-11-13 11:16:50 -0800

    Added setInterval function so that time is shown in realtime.

--- client/index.js
+++ client/index.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { render } from 'react-dom'
 import moment from 'moment'
 
-render(
-  <div>Hi, the current time is {moment().format('h:mma')}</div>,
-  document.querySelector('#app')
-)
+const currentTime = () =>
+  render(
+    <div>Hi, the current time is {moment().format('h:mm:ssa')}</div>,
+    document.querySelector('#app')
+  )
+
+setInterval(currentTime, 16)
